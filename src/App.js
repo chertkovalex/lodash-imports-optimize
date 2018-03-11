cb3cf7abb897a431b34c6e114cc6db2173563e80
chertkovalex <chertkovalex@gmail.com>
2018-03-11 16:29:04 +0200

    Add Lodash. Use one function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
 
-import DefaultView from "./components/default";
+import LodashUsage from "./components/import-full-one-function";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <DefaultView />
+        <LodashUsage />
       </div>
     );
   }
---
+++ src/components/import-full-one-function.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import _ from "lodash";
+
+export const numbers = [1, 5, 8, 10, 1, 5, 15, 42, 5];
+
+export const printArrayInline = arr => {
+  return (
+    <span>
+      [
+      {_.map(arr, (n, i) => {
+        if (i < arr.length - 1) {
+          return n + ", ";
+        }
+        return n + "";
+      })}
+      ]
+    </span>
+  );
+};
+
+export default class LodashUsage extends Component {
+  render() {
+    const printableNumbers = printArrayInline(numbers);
+    return (
+      <div>
+        <h2>const numbers = {printableNumbers};</h2>
+      </div>
+    );
+  }
+}
